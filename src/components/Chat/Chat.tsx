1ce8475b8e1ef78dd80debd4eb4ea0e491918b50
Bartek <stebart@tlen.pl>
2023-07-26 01:26:51 +0200

    add delete button

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -5,6 +5,8 @@ import {
   QuerySnapshot,
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
@@ -16,13 +18,15 @@ const Chatstyle = styled.div`
   width: 100%;
   height: 100%;
   background: lightgrey;
-  padding-left: 330px;
+  padding-left: 300px;
   overflow: auto;
   overflow-x: hidden;
+
   .message {
-    margin: 5px;
+    margin: 5px 0;
     padding: 18px;
     width: 100%;
+    word-break: break-word;
   }
 
   .sent {
@@ -36,14 +40,82 @@ const Chatstyle = styled.div`
   }
 `;
 
-const MessageContainer = styled.div``;
+const ContainerMessage = styled.div`
+  width: 100%;
+`;
 
+const TextMessage = styled.div`
+  width: 100%;
+`;
+
+const TimeMessage = styled.div`
+  width: 100%;
+`;
+
+const ChatSentContainer = styled.div`
+  margin-top: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const ChatInput = styled.input`
+  margin-left: 5px;
+  padding: 18px;
+  border-radius: 2px;
+  border: none;
+  width: 1225px;
+  text-decoration: none;
+  height: 50px;
+  outline: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease;
+  position: fixed;
+  bottom: 0;
+  color: #ffff;
+  background: #282f39;
+
+  &:focus {
+    box-shadow: 0px 0px 24px #f08b62;
+  }
+`;
+
+const ChatButton = styled.div`
+  position: fixed;
+  bottom: 10px;
+  right: 210px;
+  svg {
+    cursor: pointer;
+    align-items: center;
+    display: flex;
+  }
+`;
+
+const DeleteButton = styled.div`
+  svg {
+    cursor: pointer;
+    width: 15px;
+    height: 15px;
+  }
+`;
+
+const TimeButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    margin-left: 10px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+`;
 const Chat: React.FC = () => {
   const [user, setUser] = useState<any | null>(null);
   const [messages, setMessages] = useState<any[]>([]);
   const [newMessage, setNewMessage] = useState("");
 
-  const [currentUserUid, setCurrentUserUid] = useState<string | null>(null);
+  // const [currentUserUid, setCurrentUserUid] = useState<string | null>(null);
 
   const messageContainerRef = useRef<HTMLDivElement>(null);
 
@@ -115,9 +187,9 @@ const Chat: React.FC = () => {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
-      if (user) {
-        setCurrentUserUid(user.uid);
-      }
+      // if (user) {
+      //   setCurrentUserUid(user.uid);
+      // }
     });
 
     return () => unsubscribe();
@@ -136,34 +208,83 @@ const Chat: React.FC = () => {
     return `${day}.${month}.${year} ${hours}:${minutes}`;
   };
 
+  const deleteMessage = async (messageId: string) => {
+    try {
+      await deleteDoc(doc(db, "messages", messageId));
+
+      // Usuń wiadomość z lokalnego stanu
+      setMessages((prevMessages) =>
+        prevMessages.filter((message) => message.id !== messageId)
+      );
+    } catch (error) {
+      console.error("Error deleting message:", error);
+    }
+  };
+
   return (
     <Layout>
       <Chatstyle ref={messageContainerRef}>
-        <MessageContainer>
+        <ContainerMessage>
           {messages
             .slice()
             .reverse()
             .map((message) => (
-              <div
+              <TextMessage
                 key={message.id}
                 className={`message ${
                   message.sender === user?.email ? "sent" : "received"
                 }`}
               >
                 <strong>{message.sender}</strong>: {message.content}
-                <div>{formatTimestamp(message.timestamp)}</div>
-              </div>
+                <TimeButton>
+                  <TimeMessage>
+                    {formatTimestamp(message.timestamp)}
+                  </TimeMessage>
+                  {message.sender === user?.email && ( // Pokaż przycisk tylko jeśli wiadomość należy do zalogowanego użytkownika
+                    <DeleteButton>
+                      <svg
+                        id="Layer_1"
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        onClick={() => deleteMessage(message.id)}
+                      >
+                        <title>spam, trash, bin, garbage, delete</title>
+                        <path d="M2.88,5,5.11,24H18.89L21.12,5ZM17.11,22H6.89L5.12,7H18.88Z" />
+                        <polygon points="21 2 15 2 15 1 13 1 13 0 11 0 11 1 9 1 9 2 3 2 3 4 21 4 21 2" />
+                      </svg>
+                    </DeleteButton>
+                  )}
+                </TimeButton>
+              </TextMessage>
             ))}
-        </MessageContainer>
-        <div>
-          <input
+        </ContainerMessage>
+        <ChatSentContainer>
+          <ChatInput
             type="text"
             value={newMessage}
             onChange={(e) => setNewMessage(e.target.value)}
             onKeyDown={handleKeyPress}
           />
-          <button onClick={sendMessage}>Send</button>
-        </div>
+          <ChatButton onClick={sendMessage}>
+            <svg
+              width="25"
+              height="25"
+              viewBox="0 0 64 64"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M61.06 2.93974C60.8633 2.74408 60.6161 2.60685 60.346 2.54329C60.0759 2.47974 59.7934 2.49235 59.53 2.57974L3.53004 21.2397C3.25136 21.3373 3.00767 21.5149 2.82951 21.7504C2.65134 21.9858 2.54662 22.2686 2.52847 22.5633C2.51032 22.858 2.57954 23.1515 2.72747 23.407C2.87539 23.6625 3.09544 23.8687 3.36004 23.9997L28.21 35.7897L40 60.6397C40.1209 60.8959 40.3118 61.1127 40.5507 61.2649C40.7896 61.4171 41.0668 61.4985 41.35 61.4997H41.44C41.7371 61.4836 42.0227 61.38 42.2611 61.2021C42.4995 61.0242 42.68 60.7799 42.78 60.4997L61.44 4.49974C61.531 4.23043 61.5436 3.9408 61.4763 3.66461C61.4091 3.38842 61.2647 3.13703 61.06 2.93974V2.93974Z"
+                fill="#97CCEF"
+              />
+              <path
+                d="M60.79 2.71988C60.5015 2.54438 60.1624 2.47069 59.827 2.51057C59.4917 2.55045 59.1794 2.70161 58.94 2.93988L28.29 33.5799C28.0688 33.8029 27.9222 34.089 27.8703 34.3988C27.8184 34.7086 27.8637 35.0269 28 35.3099L40 60.6399C40.1216 60.8976 40.3141 61.1154 40.5549 61.2677C40.7958 61.42 41.0751 61.5005 41.36 61.4999H41.45C41.746 61.4844 42.0307 61.381 42.2676 61.2028C42.5045 61.0247 42.6829 60.78 42.78 60.4999L61.45 4.49988C61.5622 4.17259 61.5577 3.81658 61.4374 3.49219C61.3172 3.16779 61.0884 2.89495 60.79 2.71988V2.71988Z"
+                fill="#0099D6"
+              />
+            </svg>
+          </ChatButton>
+        </ChatSentContainer>
       </Chatstyle>
     </Layout>
   );
